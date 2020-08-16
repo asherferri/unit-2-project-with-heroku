b3a12d243038c38aaa49af78ffde28372e418c43
Asher Ferri <asher@Ashers-MacBook-Pro.local>
2020-08-16 19:25:00 -0400

    AUTH tested, redirects register/login updated to launches index

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,16 +1,20 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 
 const usersController = {
   index(req, res, next) {
-    res.json({
-      message: 'Loginn wooorrkkkssss !!!!',
-      user: req.user,
-    })
+    //redirect spits pckgjson for testing
+    // res.json({
+    //   message: 'Loginn wooorrkkkssss !!!!',
+    //   user: req.user,
+    // })
+    //now redirects us to launches main screen
+    res.redirect('/launches')
+
   },
   create(req, res, next) {
-    const salt = bcrypt.genSaltSync();
-    const hash = bcrypt.hashSync(req.body.password, salt);
+    const salt = bcrypt.genSaltSync()
+    const hash = bcrypt.hashSync(req.body.password, salt)
     new User({
       username: req.body.username,
       email: req.body.email,
@@ -19,12 +23,16 @@ const usersController = {
       .save()
       .then((user) => {
         req.login(user, (err) => {
-          if (err) return next(err);
-          res.redirect('/user');
-        });
+          if (err) return next(err)
+          //we declare wich view we land after signing up
+          //we dont want it to land on an objext view.
+          //redirect to spit user created for testing
+          //res.redirect('/user')
+          res.redirect('/launches')
+        })
       })
-      .catch(next);
+      .catch(next)
   },
-};
+}
 
-module.exports = usersController;
+module.exports = usersController
